de09242ff12ae4f77dd0b0011ec3802a1ba7e498
Jian5561 <weijian5561@gmail.com>
2018-12-28 11:15:53 +0800

    missile bug

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@
 GLuint texture = 0;
 BITMAP BMP;
 HBITMAP hBMP = NULL;
+GLUquadricObj *var = NULL;
+GLuint* textures = new GLuint[4];
+BITMAP image[4];
 
 //leftWeapon
 float leftWeaponAngle = -60.0f;
@@ -32,6 +35,7 @@ float rightWeaponTranslateY = 2.5f;
 float rightWeaponTranslateZ = -0.5f;
 
 double rotateCam = 0;
+boolean x = false;
 
 LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -54,12 +58,17 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 
 		}
 		else if (wParam == VK_UP) {
-
+			if (!x) {
+				x = true;
+			}
 		}
 		else if (wParam == 0x52) {
 		}
 
 		else if (wParam == VK_SPACE) {
+			if (x) {
+				x = false;
+			}
 
 		}
 		else if (wParam == 0x58) {
@@ -135,7 +144,7 @@ void cylinder(float topRadius, float btmRadius, float height) {
 	}
 	glEnd();
 
-	glTranslatef(0.0f,0.0f,height);
+	glTranslatef(0.0f, 0.0f, height);
 	glBegin(GL_TRIANGLE_FAN);
 	glVertex2f(x1, y1);
 	for (angle = 1.0f; angle <= 360.0f; angle += 0.2f) {
@@ -167,7 +176,7 @@ void pyramid(float height, float width, float length) {
 
 	//face2
 	glBegin(GL_TRIANGLES);
-	glVertex3f((length /2), (width/2), height);
+	glVertex3f((length / 2), (width / 2), height);
 	glVertex3f(0.0f, width, 0.0f);
 	glVertex3f(0.0f, 0.0f, 0.0f);
 	glEnd();
@@ -192,7 +201,7 @@ void pyramid(float height, float width, float length) {
 
 }
 
-void cube(float maxHeight, float maxWidth, float maxLength ) {
+void cube(float maxHeight, float maxWidth, float maxLength) {
 	glBegin(GL_QUADS);
 	//face1
 	glVertex3f(0.0f, 0.0f, maxHeight);
@@ -327,7 +336,7 @@ void leftWeapon() {
 
 	glPushMatrix();
 
-	glColor3f(192/255.0, 192 / 255.0, 192 / 255.0);
+	glColor3f(192 / 255.0, 192 / 255.0, 192 / 255.0);
 	cylinder(0.3, 0.3, 1.8);
 
 	glColor3f(211 / 255.0, 211 / 255.0, 211 / 255.0);
@@ -360,14 +369,14 @@ void rightWeapon() {
 void body() {
 	glPushMatrix();
 	glTranslatef(-3, -3, -2);
-	glRotatef(20.0f,1,0,0);
+	glRotatef(20.0f, 1, 0, 0);
 	glColor3f(0, 180 / 255.0, 45 / 1);
 	cube(3, 6, 6);
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(0, 0, -1);
-	glColor3f(160/255.0, 82 / 255.0, 45 / 255.0);
+	glColor3f(160 / 255.0, 82 / 255.0, 45 / 255.0);
 	sphere(3.0);
 	glPopMatrix();
 }
@@ -382,8 +391,8 @@ void rightHand() {
 	glPushMatrix();
 	glTranslatef(3.5f, -0.8f, 0);
 	glColor3f(160 / 255.0, 82 / 255.0, 45 / 255.0);
-	glRotatef(90.0f,1,0,0);
-	cylinder(0.3,0.3,0.5);
+	glRotatef(90.0f, 1, 0, 0);
+	cylinder(0.3, 0.3, 0.5);
 	glPopMatrix();
 
 	glPushMatrix();
@@ -395,7 +404,7 @@ void rightHand() {
 	glPushMatrix();
 	glTranslatef(3.15f, -1.7f, 1.9f);
 	glColor3f(160 / 255.0, 82 / 255.0, 45 / 255.0);
-	cube(0.75, 0.75,0.75);
+	cube(0.75, 0.75, 0.75);
 	glPopMatrix();
 
 	glPushMatrix();
@@ -470,7 +479,7 @@ void leftHand() {
 
 	glPushMatrix();
 	glTranslatef(-3.5f, -1.0f, 2.3);
-	glRotatef(-90.0f,1,0,0);
+	glRotatef(-90.0f, 1, 0, 0);
 	glColor3f(211 / 255.0, 211 / 255.0, 211 / 255.0);
 	cylinder(0.1, 0.001, 0.3);
 	glPopMatrix();
@@ -494,7 +503,7 @@ void leftLeg() {
 	glPushMatrix();
 	glTranslatef(-2.5f, -6.0, -1);
 	glColor3f(0, 270 / 255.0, 45 / 1);
-	cube(4.0,1.0,2.0);
+	cube(4.0, 1.0, 2.0);
 	glPopMatrix();
 }
 
@@ -528,7 +537,7 @@ void head() {
 
 	glPushMatrix();
 	glTranslatef(0.5, 3.5, 2);
-	glRotatef(-90.0f,1,0,0);
+	glRotatef(-90.0f, 1, 0, 0);
 
 	glColor3f(0, 150 / 255.0, 45 / 1);
 	pyramid(1.0, 0.5, 0.5);
@@ -542,27 +551,93 @@ void head() {
 	glPopMatrix();
 }
 
+void missile() {
+
+	//texture missile cone
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	HBITMAP hBMP = (HBITMAP)LoadImage(GetModuleHandle(NULL), "grey.bmp", IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
+	GetObject(hBMP, sizeof(BMP), &BMP);
+
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
+	gluQuadricTexture(var, GL_TRUE);
+
+	//missile cone
+	glPushMatrix();
+	glRotatef(45, 1, 0, 0);
+	glTranslatef(0, 0, -0.7);
+	cylinder(0.001,0.5,0.7);
+	//gluCylinder(var, 0.001, 0.5, 0.7, 20, 20);
+	glPopMatrix();
+
+
+	//texture missile cylinder
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	hBMP = (HBITMAP)LoadImage(GetModuleHandle(NULL), "blue.bmp", IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
+	GetObject(hBMP, sizeof(BMP), &BMP);
+
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
+	gluQuadricTexture(var, GL_TRUE);
+
+	//missile cylinder
+	glPushMatrix();
+	glRotatef(-135, 1, 0, 0);
+	glTranslatef(0, 0, -2);
+	gluCylinder(var, 0.5, 0.5, 2, 20, 20);
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(-135, 1, 0, 0);
+	glTranslatef(0, 0, -2);
+	gluSphere(var, 0.5, 20, 20);
+	glPopMatrix();
+
+	glDisable(GL_TEXTURE_2D);
+	DeleteObject(hBMP);
+	glDeleteTextures(1, &texture);
+}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
 
-
-
 	glPushMatrix();
-
 	glRotatef(rotateCam, 0, 1, 0);
 	glPushMatrix();
-	leftWeapon();
-	rightWeapon();
-	head();
-	body();
-	leftHand();
-	rightHand();
-	leftLeg();
-	rightLeg();
+	//leftWeapon();
+	//rightWeapon();
+
+	//
+
+	//head();
+	//body();
+	//leftHand();
+	//rightHand();
+	//leftLeg();
+	//rightLeg();
+	missile();
 	glPopMatrix();
 	glPopMatrix();
+	
+	
+	/*
+	if (x) {
+		glTranslatef(0.0f, 0.1, -0.1);
+
+	}
+	else {
+		glLoadIdentity();
+	}*/
 
 
 }
@@ -611,8 +686,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-7.0f, 7.0f,-7.0f, 5.0f,-5.0f, 5.0f);
-
+	glOrtho(-7.0f, 7.0f, -7.0f, 5.0f, -5.0f, 5.0f);
+	glMatrixMode(GL_MODELVIEW);
 	while (true)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
@@ -633,5 +708,3 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 	return true;
 }
 //--------------------------------------------------------------------
-
-
