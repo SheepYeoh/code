c2e340097c3205e76c9ac729c4b3bcb5ba58abd4
SheepYeoh <sheepyeong@gmail.com>
2019-01-01 21:14:20 +0800

    Angry mode
    need change color for angry mode

--- main.cpp
+++ main.cpp
@@ -34,6 +34,40 @@ float rightWeaponTranslateX = 1.5f;
 float rightWeaponTranslateY = 2.5f;
 float rightWeaponTranslateZ = -0.5f;
 
+//blue RGB
+float blue1R = 0;
+float blue1G = 180/255.0;
+float blue1B = 255/255.0;
+
+float blue2R = 0;
+float blue2G = 150 / 255.0;
+float blue2B = 45 / 1.0;
+
+float blue3R = 0;
+float blue3G = 64 / 255.0;
+float blue3B = 255/255.0;
+
+//anger rgb
+float anger1R = 197/255.0;
+float anger1G = 139 / 255.0;
+float anger1B = 231/255.0;
+
+float anger3R = 163/255.0;
+float anger3G = 51 / 255.0;
+float anger3B = 229 / 255.0;
+
+//angerFrequency
+float freq1 = anger1R - blue1R;
+float freq2 = anger1G - blue1G;
+float freq3 = anger1B - blue1B;
+
+float freq4 = anger3R - blue3R;
+float freq5 = anger3G - blue3G;
+float freq6 = anger3B - blue3B;
+
+boolean angry = false;
+boolean enableAnger = true;
+
 double rotateCam = 0;
 boolean x = false;
 
@@ -55,7 +89,10 @@ LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam
 		}
 
 		else if (wParam == VK_DOWN) {
-
+			if (enableAnger) {
+				enableAnger = false;
+				angry = true;
+			}
 		}
 		else if (wParam == VK_UP) {
 			if (!x) {
@@ -155,6 +192,20 @@ void cylinder(float topRadius, float btmRadius, float height) {
 	glEnd();
 }
 
+void nose(float topRadius, float btmRadius, float height) {
+	float x1 = 0.0, y1 = 0.0, x2, y2;
+	float angle;
+	double radius = 0.2;
+
+	GLUquadricObj *cylinder = NULL;
+	cylinder = gluNewQuadric();
+
+	gluQuadricDrawStyle(cylinder, GL_FILL);
+	gluCylinder(cylinder, topRadius, btmRadius, height, 3, 10);
+
+	gluDeleteQuadric(cylinder);
+}
+
 void sphere(float radius) {
 
 	GLUquadricObj *sphere = NULL;
@@ -240,9 +291,39 @@ void cube(float maxHeight, float maxWidth, float maxLength) {
 	glVertex3f(maxLength, maxWidth, 0.0f);
 	glEnd();
 
+	
+
 }
 
 void cube(float maxHeight, float maxWidth, float maxLength, const char bitmap[]) {
+	
+
+	glBegin(GL_QUADS);
+	glColor3f(blue1R, blue1G, blue1B);
+	//face1
+	glVertex3f(0.0f, 0.0f, maxHeight);
+	glVertex3f(maxLength, 0.0f, maxHeight);
+	glVertex3f(maxLength, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+
+	glEnd();
+	glBegin(GL_QUADS);
+	glColor3f(blue1R, blue1G, blue1B);
+	//face2
+	glVertex3f(0.0f, maxWidth, maxHeight);
+	glVertex3f(0.0f, 0.0f, maxHeight);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, maxWidth, 0.0f);
+	glEnd();
+	glBegin(GL_QUADS);
+	glColor3f(blue1R, blue1G, blue1B);
+	//face3
+	glVertex3f(0.0f, maxWidth, 0.0f);
+	glVertex3f(maxLength, maxWidth, 0.0f);
+	glVertex3f(maxLength, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glEnd();
+
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
 	HBITMAP hBMP = (HBITMAP)LoadImage(GetModuleHandle(NULL), bitmap, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_LOADFROMFILE);
 	GetObject(hBMP, sizeof(BMP), &BMP);
@@ -253,41 +334,6 @@ void cube(float maxHeight, float maxWidth, float maxLength, const char bitmap[])
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
-
-	glBegin(GL_QUADS);
-	//face1
-	glTexCoord2f(0.0, 1.0);
-	glVertex3f(0.0f, 0.0f, maxHeight);
-	glTexCoord2f(1.0, 1.0);
-	glVertex3f(maxLength, 0.0f, maxHeight);
-	glTexCoord2f(1.0, 0.0);
-	glVertex3f(maxLength, 0.0f, 0.0f);
-	glTexCoord2f(0.0, 0.0);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-
-	glEnd();
-	glBegin(GL_QUADS);
-	//face2
-	glTexCoord2f(0.0, 1.0);
-	glVertex3f(0.0f, maxWidth, maxHeight);
-	glTexCoord2f(1.0, 1.0);
-	glVertex3f(0.0f, 0.0f, maxHeight);
-	glTexCoord2f(1.0, 0.0);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glTexCoord2f(0.0, 0.0);
-	glVertex3f(0.0f, maxWidth, 0.0f);
-	glEnd();
-	glBegin(GL_QUADS);
-	//face3
-	glTexCoord2f(0.0, 1.0);
-	glVertex3f(0.0f, maxWidth, 0.0f);
-	glTexCoord2f(1.0, 1.0);
-	glVertex3f(maxLength, maxWidth, 0.0f);
-	glTexCoord2f(1.0, 0.0);
-	glVertex3f(maxLength, 0.0f, 0.0f);
-	glTexCoord2f(0.0, 0.0);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glEnd();
 	glBegin(GL_QUADS);
 	//face4*
 	glTexCoord2f(0.0, 0.0);
@@ -299,32 +345,27 @@ void cube(float maxHeight, float maxWidth, float maxLength, const char bitmap[])
 	glTexCoord2f(1.0, 0.0);
 	glVertex3f(0.0f, 0.0f, maxHeight);
 	glEnd();
-	glBegin(GL_QUADS);
-	//face5*
-	glTexCoord2f(0.0, 0.0);
-	glVertex3f(0.0f, maxWidth, maxHeight);
-	glTexCoord2f(1.0, 0.0);
-	glVertex3f(maxLength, maxWidth, maxHeight);
-	glTexCoord2f(1.0, 1.0);
-	glVertex3f(maxLength, maxWidth, 0.0f);
-	glTexCoord2f(0.0, 1.0);
-	glVertex3f(0.0f, maxWidth, 0.0f);
-	glEnd();
-	glBegin(GL_QUADS);
-	//face6
-	glTexCoord2f(0.0, 0.0);
-	glVertex3f(maxLength, maxWidth, maxHeight);
-	glTexCoord2f(1.0, 0.0);
-	glVertex3f(maxLength, 0.0f, maxHeight);
-	glTexCoord2f(1.0, 1.0);
-	glVertex3f(maxLength, 0.0f, 0.0f);
-	glTexCoord2f(0.0, 1.0);
-	glVertex3f(maxLength, maxWidth, 0.0f);
-	glEnd();
-
 	glDisable(GL_TEXTURE_2D);
 	DeleteObject(hBMP);
 	glDeleteTextures(1, &texture);
+
+	glBegin(GL_QUADS);
+	glColor3f(blue1R, blue1G, blue1B);
+	//face5*
+	glVertex3f(0.0f, maxWidth, maxHeight);
+	glVertex3f(maxLength, maxWidth, maxHeight);
+	glVertex3f(maxLength, maxWidth, 0.0f);
+	glVertex3f(0.0f, maxWidth, 0.0f);
+	glEnd();
+	glBegin(GL_QUADS);
+	glColor3f(blue1R, blue1G, blue1B);
+	//face6
+	glVertex3f(maxLength, maxWidth, maxHeight);
+	glVertex3f(maxLength, 0.0f, maxHeight);
+	glVertex3f(maxLength, 0.0f, 0.0f);
+	glVertex3f(maxLength, maxWidth, 0.0f);
+	glEnd();
+
 }
 
 
@@ -368,14 +409,14 @@ void rightWeapon() {
 
 void body() {
 	glPushMatrix();
+	glColor3f(blue1R, blue1G, blue1B);
 	glTranslatef(-3, -3, -2);
 	glRotatef(20.0f, 1, 0, 0);
-	glColor3f(0, 180 / 255.0, 45 / 1);
 	cube(3, 6, 6);
 	glPopMatrix();
 
 	glPushMatrix();
-	glTranslatef(0, 0, -1);
+	glTranslatef(0, -0.15, -1);
 	glColor3f(160 / 255.0, 82 / 255.0, 45 / 255.0);
 	sphere(3.0);
 	glPopMatrix();
@@ -384,7 +425,7 @@ void body() {
 void rightHand() {
 	glPushMatrix();
 	glTranslatef(3.4f, 0, 0);
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	sphere(0.8);
 	glPopMatrix();
 
@@ -397,7 +438,7 @@ void rightHand() {
 
 	glPushMatrix();
 	glTranslatef(3.5f, -1.3f, 0);
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	cylinder(0.3, 0.5, 2.0);
 	glPopMatrix();
 
@@ -436,7 +477,7 @@ void rightHand() {
 void leftHand() {
 	glPushMatrix();
 	glTranslatef(-3.4f, 0, 0);
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	sphere(0.8);
 	glPopMatrix();
 
@@ -449,7 +490,7 @@ void leftHand() {
 
 	glPushMatrix();
 	glTranslatef(-3.5f, -1.3f, 0);
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	cylinder(0.3, 0.5, 2.0);
 	glPopMatrix();
 
@@ -489,41 +530,41 @@ void leftLeg() {
 	glPushMatrix();
 	glTranslatef(-1.5f, -4.0, 0);
 
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	sphere(0.7);
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(-1.5f, -5.0, 0);
 
-	glColor3f(0, 180 / 255.0, 45 / 1);
+	glColor3f(blue1R, blue1G, blue1B);
 	sphere(0.8);
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(-2.5f, -6.0, -1);
-	glColor3f(0, 270 / 255.0, 45 / 1);
-	cube(4.0, 1.0, 2.0);
+	glColor3f(blue3R, blue3G, blue3B);
+	cube(3.0, 1.0, 2.0);
 	glPopMatrix();
 }
 
 void rightLeg() {
 	glPushMatrix();
 	glTranslatef(1.5f, -4.0, 0);
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	sphere(0.7);
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(1.5f, -5.0, 0);
-	glColor3f(0, 180 / 255.0, 45 / 1);
+	glColor3f(blue1R, blue1G, blue1B);
 	sphere(0.8);
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(0.5f, -6.0, -1);
-	glColor3f(0, 270 / 255.0, 45 / 1);
-	cube(4.0, 1.0, 2.0);
+	glColor3f(blue3R, blue3G, blue3B);
+	cube(3.0, 1.0, 2.0);
 	glPopMatrix();
 }
 
@@ -531,7 +572,7 @@ void head() {
 	glPushMatrix();
 	glTranslatef(-1.0f, 1.5, 1.3);
 
-	glColor3f(0, 64 / 255.0, 45 / 1);
+	glColor3f(blue3R, blue3G, blue3B);
 	cube(2.0, 2.0, 2.0);
 	glPopMatrix();
 
@@ -539,15 +580,49 @@ void head() {
 	glTranslatef(0.5, 3.5, 2);
 	glRotatef(-90.0f, 1, 0, 0);
 
-	glColor3f(0, 150 / 255.0, 45 / 1);
+	glColor3f(blue1R, blue1G, blue1B);
 	pyramid(1.0, 0.5, 0.5);
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(-1, 3.5, 2);
 	glRotatef(-90.0f, 1, 0, 0);
-	glColor3f(0, 150 / 255.0, 45 / 1);
+	glColor3f(blue1R, blue1G, blue1B);
 	pyramid(1.0, 0.5, 0.5);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.5, 2.8, 2.9);
+	glRotatef(-90.0f, 1, 0, 0);
+	glColor3f(0,0,0);
+	sphere(0.5);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.5, 2.8, 2.9);
+	glRotatef(-90.0f, 1, 0, 0);
+	glColor3f(0, 0, 0);
+	sphere(0.5);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.5, 2.8, 3.225);
+	glRotatef(-90.0f, 1, 0, 0);
+	glColor3f(1,1,1);
+	sphere(0.2);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.5, 2.8, 3.225);
+	glRotatef(-90.0f, 1, 0, 0);
+	glColor3f(1,1,1);
+	sphere(0.2);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0, 2.3, 3.25);
+	glColor3f(160 / 255.0, 82 / 255.0, 45 / 255.0);
+	nose(0.4,0,0.2);
 	glPopMatrix();
 }
 
@@ -676,6 +751,59 @@ void rightMissile() {
 }
 void display()
 {
+	if (!angry) {
+		if (blue1R >= 0 / 255.0) {
+			blue1R -= freq1/1000;
+		}
+		if (blue1G <= 180 / 255.0) {
+			blue1G -= freq2/ 1000;
+		}
+		if (blue1B >= 255/255.0) {
+			blue1B -= freq3/ 1000;
+		}
+
+		if (blue3R >= 0 / 255.0) {
+			blue3R -= freq4/ 1000;
+		}
+		if (blue3G <= 64 / 255.0) {
+			blue3G -= freq5/ 1000;
+		}
+		if (blue3B >= 255/255.0) {
+			blue3B -= freq6/ 1000;
+		}
+
+		if (blue1R <= 0 / 255.0&& blue1G >= 180 / 255.0 && blue1B >= 255/255.0 &&
+			blue3R <= 0 / 255.0&& blue3G >= 64 / 255.0 && blue3B >= 255 / 255.0) {
+			enableAnger = true;
+		}
+	}
+	else {
+
+		if (blue1R <= anger1R) {
+			blue1R += freq1/20;
+		}
+		if (blue1G >= anger1G) {
+			blue1G += freq2/20;
+		}
+		if (blue1B <= anger1B) {
+			blue1B += freq3/20;
+		}
+
+		if (blue3R <= anger3R) {
+			blue3R += freq4/20;
+		}
+		if (blue3G >= anger3G) {
+			blue3G +=freq5/20;
+		}
+		if (blue3B <= anger3B) {
+			blue3B += freq6/20;
+		}
+
+		if(blue1R >= anger1R && blue1G <= anger1G && blue1B >=anger1B &&
+			blue3R >= anger3R && blue3G <= anger3G && blue3B >= anger3B) {
+			angry = false;
+		}
+	}
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
 
@@ -757,7 +885,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-7.0f, 7.0f, -7.0f, 5.0f, -5.0f, 5.0f);
+	glOrtho(-7.0f, 7.0f, -7.0f, 7.0f, -7.0f, 7.0f);
 	glMatrixMode(GL_MODELVIEW);
 	while (true)
 	{
